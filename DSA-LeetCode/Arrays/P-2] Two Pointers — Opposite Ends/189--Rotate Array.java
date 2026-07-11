df42569d46a1373e0c6e722bce5982886f5c19f5
Vatsalya Singh <vatsalya2107@gmail.com>
2026-07-11 12:01:25 +0530

    Arrays-Two Pointers--Opposite End solutions

---
+++ DSA-LeetCode/Arrays/P-2] Two Pointers — Opposite Ends/189--Rotate Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[(i + k) % n] = nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = ans[i];
+        }
+    }
+}
