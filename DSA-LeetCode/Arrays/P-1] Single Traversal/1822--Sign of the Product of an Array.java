a793663532d3b5963b5126c050ec5d049eac1f91
Vatsalya Singh <vatsalya2107@gmail.com>
2026-07-11 11:45:41 +0530

    Arrays Single Traversal Solutions

---
+++ DSA-LeetCode/Arrays/P-1] Single Traversal/1822--Sign of the Product of an Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int negativeCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                return 0;
+            }
+            if (nums[i] < 0) {
+                negativeCount++;
+            }
+        }
+        if (negativeCount % 2 == 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
