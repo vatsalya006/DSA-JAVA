bd3a85b0afc7ab0060925dd695b0cc278cf53182
Vatsalya Singh <vatsalya2107@gmail.com>
2026-07-19 00:29:53 +0530

    Basic Maths - Core Maths Pattern Added

---
+++ DSA-LeetCode/Basic Maths of DSA/Core maths patterns/204--Count Primes.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime, true);
+        prime[0] = false;
+        prime[1] = false;
+
+        // Mark multiples as non-prime
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
