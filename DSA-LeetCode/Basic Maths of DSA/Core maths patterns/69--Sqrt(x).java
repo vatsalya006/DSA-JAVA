59a63e280cc405d2fe537dc19884e90c998b0c21
Vatsalya Singh <vatsalya2107@gmail.com>
2026-07-20 23:39:58 +0530

    Basic Maths - Core Maths more questions added

---
+++ DSA-LeetCode/Basic Maths of DSA/Core maths patterns/69--Sqrt(x).java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        int left = 1;
+        int right = x;
+        int ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if ((long) mid * mid == x) {
+                return mid;
+            } 
+            else if ((long) mid * mid < x) {
+                ans = mid;
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
